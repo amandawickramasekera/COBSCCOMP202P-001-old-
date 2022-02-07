54fa4f48cc57351030f17f3079f5153eb315cdf9
Amanda Wickramasekera <amandawickramasekera2000@gmail.com>
2022-02-07 12:33:15 +0530

    Initial Commit

---
+++ COBSCCOMP202P-001/COBSCCOMP202P_001App.swift
@@ -0,0 +1,17 @@
+//
+//  COBSCCOMP202P_001App.swift
+//  COBSCCOMP202P-001
+//
+//  Created by Amanda Wickramasekera on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct COBSCCOMP202P_001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COBSCCOMP202P-001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COBSCCOMP202P-001
+//
+//  Created by Amanda Wickramasekera on 2022-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
